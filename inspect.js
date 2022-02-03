cafb34eea5d12f3ae5a568ce5a60f217b7529837
Silas Campbell <92600501+Silasssssss@users.noreply.github.com>
2022-02-03 13:40:36 -0600

    V2.1
    Replace "alert()" call with custom element.

--- inspect.js
+++ inspect.js
@@ -1,8 +1,6 @@
 var i_enabled = false
 var i_enableKey = "["
 
-alert('Inspector has activated! Use "[" to toggle it!\n\nChange the keybind with "\\".\n\n~ SilasDevs 2021 V2.0 ~')
-
 window.addEventListener('keydown', function (e) {
     if (e.key == i_enableKey) {
         i_enabled = !i_enabled
@@ -16,3 +14,45 @@ window.addEventListener('keydown', function (e) {
          i_enableKey = prompt("Change keybind:")[0].toLowerCase()   
     }
 });
+
+var css = `
+.inspector-alert {
+  display: flex;
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  background-color: white;
+  align-items: center;
+  padding: 5px;
+  border-left: solid lime;
+  border-radius: 3px;
+  opacity: 1;
+  transition: opacity 200ms;
+}
+
+.inspector-alert span {
+  text-align: center;
+  color: black;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`
+var style = document.createElement('style');
+document.head.appendChild(style)
+style.type = 'text/css';
+style.appendChild(document.createTextNode(css));
+
+var alert = document.createElement('div')
+var span = document.createElement('span')
+alert.classList.add('inspector-alert')
+span.innerText = "Inspector V2.1 by SilasDevs"
+alert.appendChild(span)
+
+document.body.appendChild(alert)
+
+setTimeout(function() {
+  alert.style.opacity = 0
+  setTimeout(function() {
+    alert.remove()
+  }, 200)
+}, 5000)
+
